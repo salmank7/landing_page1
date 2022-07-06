b36769fa84414d8628684b55510b82586c96c07c
Salman Khan <salmank4954@gmail.com>
2022-07-06 03:30:55 -0700

    frequently asked questions

---
+++ main.js
@@ -0,0 +1,14 @@
+const btns = document.querySelectorAll(".q-btn");
+const ans = document.querySelectorAll('.q-para');
+
+
+
+
+btns.forEach(function(btn){
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains('plus')){
+            ans.
+        }
+    })
+})
